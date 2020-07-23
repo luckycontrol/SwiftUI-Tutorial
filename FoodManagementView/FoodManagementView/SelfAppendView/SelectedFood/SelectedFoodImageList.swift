394087bf27fc394e313f3984118f7ec6b7fdce2c
Cho - chong woon <37057884+luckycontrol@users.noreply.github.com>
2020-07-23 17:55:07 +0900

    유통기한 관리

---
+++ FoodManagementView/FoodManagementView/SelfAppendView/SelectedFood/SelectedFoodImageList.swift
@@ -0,0 +1,21 @@
+//
+//  SelectedFoodImageList.swift
+//  FoodManagementView
+//
+//  Created by 조종운 on 2020/07/22.
+//  Copyright © 2020 조종운. All rights reserved.
+//
+
+import SwiftUI
+
+struct SelectedFoodImageList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectedFoodImageList_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedFoodImageList()
+    }
+}
---
+++ FoodManagementView/FoodManagementView/SelfAppendView/SelectedFood/SelectedFoodImageView.swift
@@ -0,0 +1,21 @@
+//
+//  SelectedFoodImageView.swift
+//  FoodManagementView
+//
+//  Created by 조종운 on 2020/07/22.
+//  Copyright © 2020 조종운. All rights reserved.
+//
+
+import SwiftUI
+
+struct SelectedFoodImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectedFoodImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedFoodImageView()
+    }
+}
